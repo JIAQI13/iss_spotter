329dfd516c31c40af3f4902c60a488c8d426b234
JIAQI13 <jiaqi11@ualberta.ca>
2021-05-07 03:58:38 +0000

    iss finished

---
+++ iss_promised.js
@@ -0,0 +1,29 @@
+// iss_promised.js
+const request = require('request-promise-native');
+
+const fetchMyIP = function() {
+    return request('https://api.ipify.org?format=json');
+};
+//module.exports = { fetchMyIP};
+const fetchCoordsByIP = function(body) {
+    const ip = JSON.parse(body).ip;
+    return request(`https://freegeoip.app/json/${ip}`);
+  };
+//module.exports = { fetchMyIP, fetchCoordsByIP };
+const fetchISSFlyOverTimes = function(body) {
+    const latitude = JSON.parse(body).latitude;
+    const longitude= JSON.parse(body).longitude;
+    const url = `http://api.open-notify.org/iss-pass.json?lat=${latitude}&lon=${longitude}`;
+    return request(url);
+  };  
+//module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
+const nextISSTimesForMyLocation = function() {
+    return fetchMyIP()
+      .then(fetchCoordsByIP)
+      .then(fetchISSFlyOverTimes)
+      .then((data) => {
+        const { response } = JSON.parse(data);
+        return response;
+      });
+  };
+module.exports = { nextISSTimesForMyLocation };
